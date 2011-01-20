6763e49b52e5c22fc3bb935273546e8e18519cb9
albertwcheng <albertwcheng@gmail.com>
2011-01-20 11:11:30 -0500

    commit

--- scripts/iBowtie_merge_all.sh
+++ scripts/iBowtie_merge_all.sh
@@ -2,12 +2,23 @@
 
 source fileUtils.sh
 
-if [ $# -lt 1 ]; then
-	echo "usage: " $0 "extReadFiles[txt,fastq,...]"
+if [ $# -ne 2 ]; then
+	echo "usage: " $0 "extReadFiles[txt,fastq,...] useCluster[y|n]"
 	exit
 fi
 
 extReadFiles=$1
+useCluster=$2
+
+if [[ $useCluster == "y" ]]; then
+	echo "use cluster version active"
+elif [[ $useCluster == "n" ]]; then
+	echo "standalone version active"
+else
+	echo "I don't understand useCluster=$useCluster option. say either y for yes or n for no"
+	bash $0
+	exit
+fi
 
 cd ..
 
@@ -33,6 +44,10 @@ do
 	solFileNoExt=${solFile/.$extReadFiles/}
 	echo $solFileNoExt
 	echo "samtools merge $BAMPATHMERGED/${solFileNoExt}.bam $MAPSPATH/split_${solFile}*.bam; samtools sort $BAMPATHMERGED/${solFileNoExt}.bam  $BAMPATHMERGED/${solFileNoExt}.sorted; samtools index $BAMPATHMERGED/${solFileNoExt}.sorted.bam" > $BAMPATHMERGED/${solFileNoExt}.sh
-	bsub bash $BAMPATHMERGED/${solFileNoExt}.sh
+	if [[ $useCluster == "y" ]]; then
+		bsub bash $BAMPATHMERGED/${solFileNoExt}.sh
+	else
+		bash $BAMPATHMERGED/${solFileNoExt}.sh	
+	fi
 done
 
